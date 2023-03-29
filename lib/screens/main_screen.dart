8ea869a0048cbff9ce789ce89ec43d31d2966426
Andrii Seleznov <seleznov.andriy@gmail.com>
2023-03-29 18:04:14 +0300

    fixed that imageUrl in the MainScreen class wasn't final

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -5,7 +5,7 @@ import 'package:unsplash_client/data/models.dart';
 
 class MainScreen extends StatelessWidget {
   // Photo photo;
-  String imageUrl = 'https://images.unsplash.com/photo-1680082286331-4473a0801b0e?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=Mnw0Mjg5MzR8MHwxfGFsbHwyfHx8fHx8Mnx8MTY4MDA5NTQ1OA\u0026ixlib=rb-4.0.3\u0026q=80\u0026w=1080';
+  final String imageUrl = 'https://images.unsplash.com/photo-1680082286331-4473a0801b0e?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=Mnw0Mjg5MzR8MHwxfGFsbHwyfHx8fHx8Mnx8MTY4MDA5NTQ1OA\u0026ixlib=rb-4.0.3\u0026q=80\u0026w=1080';
   MainScreen({super.key});
 
   @override
